55da6a5c99c20d9ba89fd92c85c6cc2351350183
yinccc <349064816@qq.com>
2019-05-08 22:36:52 +0800

    20190508

---
+++ 538-20190508-Convert BST to Greater Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def __init__(self):
+        self.total = 0
+
+    def convertBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root is not None:
+            self.convertBST(root.right)
+            self.total += root.val
+            root.val = self.total
+            self.convertBST(root.left)
+        return root

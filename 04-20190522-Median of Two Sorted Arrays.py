810040237e63f9df5f520a8f8380474a9a3712c6
yinccc <349064816@qq.com>
2019-05-22 22:10:57 +0800

    20190522

---
+++ 04-20190522-Median of Two Sorted Arrays.py
@@ -0,0 +1,82 @@
+'''
+java version
+class Solution {
+    public double findMedianSortedArrays(int input1[], int input2[]) {
+        //if input1 length is greater than switch them so that input1 is smaller than input2.
+        if (input1.length > input2.length) {
+            return findMedianSortedArrays(input2, input1);
+        }
+        int x = input1.length;
+        int y = input2.length;
+
+        int low = 0;
+        int high = x;
+        while (low <= high) {
+            int partitionX = (low + high)/2;
+            int partitionY = (x + y + 1)/2 - partitionX;
+
+            //if partitionX is 0 it means nothing is there on left side. Use -INF for maxLeftX
+            //if partitionX is length of input then there is nothing on right side. Use +INF for minRightX
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : input1[partitionX - 1];
+            int minRightX = (partitionX == x) ? Integer.MAX_VALUE : input1[partitionX];
+
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : input2[partitionY - 1];
+            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : input2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                //We have partitioned array at correct place
+                // Now get max of left elements and min of right elements to get the median in case of even length combined array size
+                // or get max of left for odd length combined array size.
+                if ((x + y) % 2 == 0) {
+                    return ((double)Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY))/2;
+                } else {
+                    return (double)Math.max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) { //we are too far on right side for partitionX. Go on left side.
+                high = partitionX - 1;
+            } else { //we are too far on left side for partitionX. Go on right side.
+                low = partitionX + 1;
+            }
+        }
+
+        //Only we we can come here is if input arrays were not sorted. Throw in that scenario.
+        throw new IllegalArgumentException();
+    }
+}
+'''
+
+
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        if len(nums1) > len(nums2):
+            return self.findMedianSortedArrays(nums2, nums1)
+
+        x = len(nums1)
+        y = len(nums2)
+
+        low = 0
+        high = x
+        while low <= high:
+            partX = (low + high) / 2
+            partY = (x + y + 1) / 2 - partX
+
+            maxLeftX = float('-inf') if partX == 0 else nums1[partX - 1]
+            minRightX = float('inf') if partX == x else nums1[partX]
+
+            maxLeftY = float('-inf') if partY == 0 else nums2[partY - 1]
+            minRightY = float('inf') if partY == y else nums2[partY]
+
+            if (maxLeftX <= minRightY and maxLeftY <= minRightX):
+                if (x + y) % 2 == 0:
+                    return float((max(maxLeftX, maxLeftY) + min(minRightX, minRightY))) / 2
+                else:
+                    return max(maxLeftX, maxLeftY)
+            elif maxLeftX > minRightY:
+                high = partX - 1
+            else:
+                low = partX + 1

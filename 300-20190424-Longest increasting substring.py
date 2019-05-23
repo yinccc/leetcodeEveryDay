88310392d449d9d6c285404204f95f87b4628da5
yinccc <349064816@qq.com>
2019-05-23 15:05:55 +0800

    20190523

--- 300-20190424-Longest increasting substring.py
+++ 300-20190424-Longest increasting substring.py
@@ -12,3 +12,21 @@ class Solution:
             dp[i]=templen+1
             maxlen=max(maxlen,dp[i])
         return maxlen
+
+
+class Solution2(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        maxlen = 1
+        dp = [1] * len(nums)
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j] and dp[j] + 1 > dp[i]:
+                    dp[i] = dp[j] + 1
+            maxlen = max(maxlen, dp[i])
+        return maxlen

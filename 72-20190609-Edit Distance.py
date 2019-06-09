38220e2bc4ce9bcbb6f095b225b5d85049a32435
yinccc <349064816@qq.com>
2019-06-09 17:34:25 +0800

    20190609-2

--- 72-20190609-Edit Distance.py
+++ 72-20190609-Edit Distance.py
@@ -15,3 +15,25 @@ class Solution(object):
             for j in range(1,len2):
                 dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1]+(word1[i-1]!=word2[j-1]))
         return dp[-1][-1]
+
+
+# -*- coding:utf-8 -*-
+
+class MinCost:
+    def findMinCost(self, A, B,c0, c1, c2):
+        # write code here
+        row=len(A)+1
+        column=len(B)+1
+        dp=[[0 for x in range(column)] for y in range(row)]
+        for i in range(row):
+            dp[i][0]=c1*i
+        for j in range(column):
+            dp[0][j]=c0*j
+        for i in range(1,row):
+            for j in range(1,column):
+                if A[i-1]==B[j-1]:
+                    dp[i][j]=dp[i-1][j-1]
+                else:
+                    dp[i][j]=dp[i-1][j-1]+c2
+                dp[i][j]=min(dp[i][j],dp[i][j-1]+c0,dp[i-1][j]+c1)
+        return dp[-1][-1]
